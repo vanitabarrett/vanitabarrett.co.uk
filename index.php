560a392237f405845b48dc8d2fd71731d7e3153e
Vanita Barrett <vanitabarrett@gds6198.home>
2018-01-20 17:33:31 +0000

    Add mobile viewport and meta tags

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Vanita Barrett: Portfolio</title>
-    <link rel="shortcut icon" href="favicon.ico" />
+    <title>Vanita Barrett: Web Developer</title>
+    <meta charset="utf-8" >
+    <meta property="og:title" content="Vanita Barrett: Web Developer" />
+    <meta property="og:description" content="Vanita Barrett: Web Developer portfolio. Currently Frontend Developer at Government Digital Service." />
+    <meta name="robots" content="index, follow" />
+    <meta name="keywords" content="vanita, barrett, vanita barrett, website, developer, designer, web, surrey, university, portfolio, frontend, frontend developer, gds, govuk" />
+    <meta name="description" content="Vanita Barrett: Web Developer portfolio. Currently Frontend Developer at Government Digital Service." />
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
+    <link rel="shortcut icon" href="favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/styles/build/styles.css">
