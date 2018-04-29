be020e3e500b5099bcef448fa1a4eac4d43760aa
Vanita Barrett <vanita_barrett@hotmail.co.uk>
2018-04-29 13:59:53 +0100

    Add styled header and footer

---
+++ partials/header.inc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Vanita Barrett: Web Developer</title>
+    <meta charset="utf-8" >
+    <meta property="og:title" content="Vanita Barrett: Web Developer" />
+    <meta property="og:description" content="Vanita Barrett: Web Developer portfolio. Currently Frontend Developer at Government Digital Service." />
+    <meta name="robots" content="index, follow" />
+    <meta name="keywords" content="vanita, barrett, vanita barrett, website, developer, designer, web, surrey, university, portfolio, frontend, frontend developer, gds, govuk" />
+    <meta name="description" content="Vanita Barrett: Web Developer portfolio. Currently Frontend Developer at Government Digital Service." />
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/styles/build/styles.css">
+  </head>
+
+  <body>
+    <header>
+        <div class="max-width">
+            <a href="/" class="web-title"><span class="web-title--capital">V</span>anita <span class="web-title--capital">B</span>arrett</a>
+            <nav aria-label="main page navigation">
+                <a href="/contact.php" >Contact</a>
+                <a href="blog.php" >Blog</a>
+            </nav>
+        </div>
+    </header>
