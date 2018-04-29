a581b95b20f2d14b6f82c7db34aac27811220771
vanitabarrett <vanita_barrett@hotmail.co.uk>
2018-04-29 15:28:40 +0100

    Add template project page

---
+++ project.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('partials/header.inc.php');
+?>
+
+<nav class="breadcrumbs max-width" aria-label="breadcrumb">
+    <ol class="breadcrumbs__list">
+        <li class="breadcrumbs__list__item">
+            <a href="/">Home</a>
+        </li>
+        <li class="breadcrumbs__list__item">
+            <a href="/">Projects</a>
+        </li>
+        <li class="breadcrumbs__list__item">
+            <span>GOV.UK</span>
+        </li>
+    </ol>
+</nav>
+
+<div class="project-info max-width">
+    <h1>Project: Sushi Lab <a href="http://www.sushilab.co.uk" rel="external">(www.sushilab.co.uk)</a></h1>
+    <img class="project-image" src="assets/projects/sushilab.png" alt="GOV.UK website homepage on a desktop computer, laptop and mobile device" />
+
+    <p>This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description. This is some kind of description.</p>
+
+    <h2>Uses the following technologies:</h2>
+    <ul class="project-info__technologies">
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>jQuery</li>
+        <li>PHP</li>
+        <li>MySQL</li>
+    </ul>
+</div>
+
+<?php
+     require_once('partials/footer.inc.php');
+?>
