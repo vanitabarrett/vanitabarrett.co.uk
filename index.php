89f7fd3dcddbdda7c9ebd927367c71668b05f077
vanitabarrett <vanita_barrett@hotmail.co.uk>
2018-04-29 14:16:57 +0100

    Add intro box on home page

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
     require_once('partials/header.inc.php');
 ?>
 
+<div class="intro max-width">
+    <p>I'm a Web Developer and Designer, based in London. I work as a <span class="intro-em">Frontend Developer</span> at <a href="https://gds.blog.gov.uk/" rel="external" class="intro-em">Government Digital Service</a>, helping make <a href="https://gov.uk" rel="external" class="intro-em">GOV.UK</a> better for users.</p>
+    <p>I also <a href="/blog.php" class="intro-em">write</a> about my experiences working as a developer and being a woman in tech.</p>
+    <p>You can find me on <a href="https://www.linkedin.com/in/vanitabarrett/" rel="external" class="intro-em">LinkedIn</a> and <a href="https://twitter.com/V_Barrett" rel="external" class="intro-em">Twitter</a>.</p>
+</div>
+
 <?php
     require_once('partials/footer.inc.php');
 ?>
