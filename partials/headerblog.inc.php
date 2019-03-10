bb4e9ed0b72eced6bca4956b8278a55130430d21
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-03-10 20:09:47 +0000

    Fix blogs link from blog page

--- partials/headerblog.inc.php
+++ partials/headerblog.inc.php
@@ -39,7 +39,7 @@
 
   <body>
     <?php
-        $pages = array('/' => 'Home','blogs' =>'Blog');
+        $pages = array('/' => 'Home','/blogs' =>'Blog');
     ?>
     <div class="wrapper">
         <header>
