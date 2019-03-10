d6ea54b18107abb7bb653099179e23c94e5b648c
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-03-10 16:14:33 +0000

    Add about section to blog

--- blog.php
+++ blog.php
@@ -12,14 +12,20 @@
     else {
 ?>
   <div class="max-width">
-    <div class="share-links">
-      <p>Share on:</p>
-      <ul>
-        <li><a href="https://www.facebook.com/sharer/sharer.php?u=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>">Facebook</a></li>
-        <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>&title=<?= $blog["title"] ?>&summary=<?= $blog["description"] ?>">LinkedIn</a></li>
-        <li><a href="https://twitter.com/home?status=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>" >Twitter</a></li>
-      </ul>
-    </div>
+    <aside>
+      <div class="about">
+        <h2>About</h2>
+        <p>I'm a Web Developer, based in London. I work as a Frontend Developer at Government Digital Service, helping make GOV.UK better for users.</p>
+      </div>
+      <div class="share-links">
+        <p>Share on:</p>
+        <ul>
+          <li><a href="https://www.facebook.com/sharer/sharer.php?u=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>">Facebook</a></li>
+          <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>&title=<?= $blog["title"] ?>&summary=<?= $blog["description"] ?>">LinkedIn</a></li>
+          <li><a href="https://twitter.com/home?status=https://vanitabarrett.co.uk/blog/<?= $blog["id"] ?>" >Twitter</a></li>
+        </ul>
+      </div>
+    </aside>
 
     <div class="blog">
       <img src="/writing/images/<?= $blog["metadata"]["id"] ?>.png" class="blog__cover-image">
